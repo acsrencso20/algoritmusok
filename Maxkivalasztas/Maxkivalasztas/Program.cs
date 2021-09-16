45ad6ff626c3c2d79ea3840e93ecb37f500334fd
acsrencso20 <90681529+acsrencso20@users.noreply.github.com>
2021-09-16 13:52:29 +0200

    Add files via upload

---
+++ Maxkivalasztas/Maxkivalasztas/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Maxkivalasztas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Random veletlen = new Random();
+                      
+            int max = 0;
+            
+            int [] tomb = new int [50];
+
+            for (int i = 0; i < tomb.Length; i++)
+            {
+                tomb[i] = veletlen.Next(1, 101);
+                if (tomb[i] > max) max = tomb[i];
+                Console.Write("\t{0}",tomb[i]);
+            }
+            Console.WriteLine("\n---------------------------------------------");
+            Console.WriteLine("\n{0}",max);
+
+            Console.ReadKey();
+        }
+    }
+}
