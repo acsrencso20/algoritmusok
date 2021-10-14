49b81554a81de8606c5525cacbd58832168aed76
acsrencso20 <90681529+acsrencso20@users.noreply.github.com>
2021-10-14 13:21:45 +0200

    Add files via upload

---
+++ fibonacci/fibonacci/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fibonacci
+{
+   class Program
+
+    {
+        public static int fib(int szam)                   
+        {
+            if (szam <= 1) return 1;
+            else return fib(szam - 2) + fib(szam - 1);
+        }
+
+        static void Main(string[] args)
+
+        {
+            
+            int osszeg;
+
+            do
+
+            {
+            Console.WriteLine("Meddig adjuk össze a számokat?");
+
+            } while (!(int.TryParse(Console.ReadLine(), out osszeg)));
+
+            for (int i = 0; i < osszeg; i++) Console.Write("{0} ", fib(i));
+
+
+            Console.ReadKey();
+
+
+
+        }
+
+    }
+
+}
+
